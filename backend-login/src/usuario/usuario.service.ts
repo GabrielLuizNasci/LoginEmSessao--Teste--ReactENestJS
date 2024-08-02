f06ba5994d38b8af01fba39be8ff6f0528b70a2a
Gabriel Luiz <gabriellnsilva2004@gmail.com>
2024-08-02 16:33:13 -0300

    Preparação do servidor

--- backend-login/src/usuario/usuario.service.ts
+++ backend-login/src/usuario/usuario.service.ts
@@ -1,11 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class UsuarioService {
-  create(createUsuarioDto: CreateUsuarioDto) {
-    return 'This action adds a new usuario';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUsuarioDto: CreateUsuarioDto) {
+    const data: Prisma.UsuarioCreateInput = {
+      ...createUsuarioDto,
+    }
+
+    const UsuarioCriado = await this.prisma.usuario.create({data});
+
+    return UsuarioCriado;
   }
 
   findAll() {
